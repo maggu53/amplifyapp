5210e772bdb5688da6c04c22d699ffa8fbc8a70d
root <root@LAPTOP-CLPM7TR1.localdomain>
2021-09-11 21:10:41 +0530

    changes 5

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 		      <div className="App">
 		        <header className="App-header">
 		          <img src={logo} className="App-logo" alt="logo" />
-		          <h1>Hello from V2 chelRG ama</h1>
+		          <h1>Hello NN from V2 chelRG ama</h1>
 		        </header>
 		      </div>
 		    );
